a33a555ac8b51ea52f947b8c9a3ed170e7642c6a
Elena Cheprasova <hellogbel@gmail.com>
2017-06-21 20:33:29 +0300

    Create bheader.h
    Function signatures for initializing our bcomp memory

---
+++ bheader.h
@@ -0,0 +1,18 @@
+#ifndef _BCOMP_H_
+#define _BCOMP_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_SIZE 2048
+#define TRUE 1
+#define FALSE 0
+
+unsigned short get_address(char* title);
+unsigned short get_data(int* finish_input);
+void push(unsigned short val, unsigned short* mem,
+	unsigned short* top);
+void fill_mem(unsigned short* mem, unsigned short* top, int* finish_input);
+
+#endif
